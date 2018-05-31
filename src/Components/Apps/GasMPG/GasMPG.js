8b3f9f801d3c87a4d36d8f449d2b353c9e12a0dd
Nick Pisarski <nick.pisarski@slalom.com>
2018-05-31 12:12:43 -0500

    added gas mpg app

---
+++ src/Components/Apps/GasMPG/GasMPG.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class GasMPG extends Component{
+    
+    render(){
+        return (
+            <div className="GasMPG">
+                <div>Chart Goes Here</div>
+                <div>Data Table GOes Here</div>
+            </div>
+        );
+    }
+}
+
+export default GasMPG;
