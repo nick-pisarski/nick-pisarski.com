ec3027f593c2185b49e058adf22ce3686ecb72a0
Nick Pisarski <nick.pisarski@s00929-mbpr.2d.hq>
2018-06-03 10:12:22 -0500

    worked on chart stylings

--- src/Components/Apps/GasMPG/Chart/Chart.js
+++ src/Components/Apps/GasMPG/Chart/Chart.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
 // import Chart from 'chart.js';
-import {VictoryLine, VictoryChart, VictoryAxis, }from 'victory';
+import {VictoryLine, VictoryChart, VictoryAxis, VictoryLabel, VictoryTheme, VictoryStack }from 'victory';
 import "./Chart.css"
+
+const months= ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 class Chart extends Component{
     formatData = (y_prop) => {
@@ -16,6 +18,20 @@ class Chart extends Component{
 
     }
 
+    getAverage = (y_prop) => {
+        const {data} = this.props;
+        const sum = data.reduce((sum, next, iter) => {
+            return sum += next[y_prop];
+        }, 0);
+        return Math.round(sum / data.length);
+    }
+
+    getAverageData = (data, avg) => {
+        return data.map(item => {
+            return {x: item.x, y: avg}
+        })
+    }
+
     generateDomain = (property, min = 0) => {
         const {data} = this.props;
         const max  = data.map(item => item[property]).reduce((max, curr) => Math.max(max, curr));
@@ -27,23 +43,66 @@ class Chart extends Component{
         const y_prop = 'miles_per_gallon';
         const data = this.formatData(y_prop);
         const domain = this.generateDomain(y_prop);
+        const avgMPG = this.getAverage('miles_per_gallon');
+        const avgData = this.getAverageData(data, avgMPG);
+        console.log(avgData);
+
         
         return (
             <div className="Chart">
-                <VictoryChart scale={{ x: "time" }} domain={{y:domain}}>        
-                {/* <VictoryAxis tickFormat={x => new Date(x).getFullYear()} label="Time"/>             */}
-                    <VictoryLine
-                        data={data}
-                        style={{
-                            data: { stroke: "#c43a31" },
-                            parent: { border: "1px solid #ccc"}
-                        }}
-                        interpolation="natural"
-                        animate={{
-                            duration: 2000,
-                            onLoad: { duration: 1000 }
-                          }}
-                    />
+                <VictoryChart scale={{ x: "time" }} domain={{y:domain}}
+                    theme={VictoryTheme.material}
+                    width={800}
+                >        
+                <VictoryAxis 
+                    tickFormat={x => {
+                        const date = new Date(x);
+                        // const month = months[date.getMonth()];
+                        const month = date.getMonth() + 1;
+                        const year  = date.getFullYear().toString();
+                        return `${month}/${year[2]}${year[3]}`
+                    }} 
+                    tickCount={6}
+                    crossAxis
+                    standalone={false}
+                    />            
+                <VictoryAxis 
+                    tickFormat={y => y.toFixed(0)} 
+                    label='Miles Per Gallon'
+                    axisLabelComponent={
+                        <VictoryLabel dy={-30}/>
+                    }
+                    dependentAxis
+                    crossAxis
+                    standalone={false}
+                    />          
+                    <VictoryStack style={{
+                        parent: { border: "1px solid #ccc"},
+                        labels:{
+                            stroke: "#00ff00",
+                            fill: d => d.y  < avgMPG ? '#FF0000' : '#00FF00',
+                        }
+                    }}>
+                        <VictoryLine
+                            data={data}
+                            style={{
+                                data: { stroke: "#c43a31" },
+                                
+                            }}
+                            labels={ d => d.y}
+                            interpolation="natural"
+                            animate={{
+                                duration: 2000,
+                                onLoad: { duration: 1000 }
+                            }}
+                            />
+                        <VictoryLine
+                            data={avgData}
+                            style={{
+                                data: { stroke: "#0000ff" },
+                            }}
+                        />
+                        </VictoryStack>  
                 </VictoryChart>
 
             </div>
