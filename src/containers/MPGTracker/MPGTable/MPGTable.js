a4a7e5a13bc7d375cfe860f4b9865444dcfe909c
Nick Pisarski <nick.pisarski@gmail.com>
2018-09-26 13:27:39 -0500

    worked on table stylings, and mobile friendliness for mpg tracker

--- src/containers/MPGTracker/MPGTable/MPGTable.js
+++ src/containers/MPGTracker/MPGTable/MPGTable.js
@@ -7,27 +7,40 @@ import { Dollar, Decimal } from "@shared/ui/Masks/index";
 
 const columns = [{
             Header: 'Date',
-            accessor: 'created'
+            accessor: 'created',
+            maxWidth: 95
           },{
             Header: 'Miles',
             accessor: 'miles',
-            Cell: props => <Decimal value={props.value} places={1}/>
+            Cell: props => <Decimal value={props.value} places={1}/>,
+            maxWidth: 86
           },{
             Header: 'Miles Per Gallon',
             accessor: 'miles_per_gallon',
-            Cell: props => <Decimal value={props.value}/>
+            Cell: props => <Decimal value={props.value}/>,
+            maxWidth: 125
           },{
             Header: 'Cost Per Gallon',
             accessor: 'cost_per_gallon',
-            Cell: props => <Dollar value={props.value} />
+            Cell: props => <Dollar value={props.value} />,
+            maxWidth: 125
           },{
             Header: 'Cost',
             accessor: 'total',
-            Cell: props => <Dollar value={props.value} />
+            Cell: props => <Dollar value={props.value} />,
+            maxWidth: 86
           },{
             Header: 'Notes',
             accessor: 'notes' 
           }
 ];
 
-export default  props => <ReactTable data={props.data} loading={props.loading} columns={columns} />;
+export default  props => {
+  return (
+    <ReactTable 
+      data={props.data} 
+      loading={props.loading} 
+      columns={columns}
+      defaultPageSize={5}
+       />
+  )};
