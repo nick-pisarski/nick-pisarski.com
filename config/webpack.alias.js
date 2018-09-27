128d25dfe974111b2171c0114f36defa2582eda0
Nick Pisarski <nick.pisarski@gmail.com>
2018-09-27 10:24:31 -0500

    cleaned up webpack.alias file

---
+++ config/webpack.alias.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {      
+    // Support React Native Web
+    // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+    'react-native': 'react-native-web',
+    "@components": path.resolve(__dirname, "../src/components"),
+    "@containers": path.resolve(__dirname, "../src/containers/"),
+    "@shared": path.resolve(__dirname, "../src/components/shared"),
+    "@store": path.resolve(__dirname, "../src/store"),
+  }
